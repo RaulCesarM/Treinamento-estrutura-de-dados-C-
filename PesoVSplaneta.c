0e784d4e48b19059ea9736e0150d81227bcf3066
RaulCesar777 <raulzito737@gmail.com>
2018-12-28 20:50:12 -0200

    Create PesoVSplaneta.c
    Uma calculadora de qual seria seu peso em um respectivo planeta.

---
+++ PesoVSplaneta.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+
+void menu(){
+	int op; 
+	 float pterra;
+
+	printf("planetas para serem analizados\n");
+	printf("\n 1 para mercurio");
+	printf("\n 2 para venus");
+	printf("\n 3 para marte");
+	printf("\n 4 para jupiter");
+	printf("\n 5 para saturno");
+	printf("\n 5 para urano");
+	printf("\n escolha ");
+	scanf("%d%*c", &op);
+	printf("seu peso na terra ? \n");
+	scanf("%f", &pterra);
+	switch(op){
+		case 1:
+		printf("seu peso em mercurio %2.f kg\n",(pterra/1)*0.38);
+		break;
+		case 2:
+		printf("seu peso em venus %2.f kg\n",(pterra/1)*0.91);
+		break;
+		case 3:
+		printf("seu peso marte %2.f kg\n",(pterra/1)*0.38);
+		break;
+		case 4:
+		printf("seu peso jupiter %2.f kg\n",(pterra/1)*2.34);
+		break;
+		case 5:
+		printf("seu peso saturno %2.f kg \n",(pterra/1)*1.06);
+		break;
+		case 6:
+		printf("seu peso urano %2.f kg \n",(pterra/1)*1.19);
+		break;
+		default:
+		printf("opcao inexistente");
+		break;
+
+	}
+
+
+}
+
+
+int main()
+{
+	
+ menu();
+
+	return 0;
+}
