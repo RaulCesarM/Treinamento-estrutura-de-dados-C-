f99d03be1e5d5533adfa0e70db73fc8e747040d4
RaulCesar777 <raulzito737@gmail.com>
2018-12-28 20:01:29 -0200

    Create Calculadora.c
    Uma calculadora Simples.

---
+++ Calculadora.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+int main ()
+{
+	char resp [2];
+	float a,b,s;
+
+
+	printf("\n\n\n\t\t\t\t*************\n");
+	printf("\t\t\t\t*CALCULADORA*\n");
+	printf("\t\t\t\t*************\n");
+	printf("\n\t\t\t\t+ para somar\n");
+	printf("\n\t\t\t\t- para subtrair\n");
+	printf("\n\t\t\t\t* para multiplicar\n");
+    printf("\n\t\t\t\t/ para dividir \n");
+    printf("\n\t\t\t\tdigite a opcao \n");
+    gets(resp);
+
+    if (!strcmp(resp,"+"))
+    {
+    	printf("digite o 1 numero \n");
+    	scanf("%f", &a);
+    	printf("digite o 2 numero \n");
+    	scanf("%f", &b);
+    	s=a+b;
+    	printf("a resposta eh %f \n" ,s);
+    }
+    else {
+    	if(!strcmp(resp,"-")){
+
+    		printf("digite o 1 numero \n");
+    		scanf("%f", &a);
+    		printf("digite o 2 numero \n");
+    		scanf("%f", &b);
+    		s=a-b;
+    		printf("a resposta eh %f \n" ,s);
+ 		}
+    		else{
+    			if (!strcmp(resp,"*")){
+    				printf("digite o 1 numero \n");
+    				scanf("%f", &a);
+    				printf("digite o 2 numero \n");
+    				scanf("%f", &b);
+    				s=a*b;
+    				printf("a resposta eh %f \n" ,s);
+    			}
+    				else{
+    					if (!strcmp(resp,"*")){
+    						printf("digite o 1 numero \n");
+    						scanf("%f", &a);
+    						printf("digite o 2 numero \n");
+    						scanf("%f", &b);
+    						s=a*b;
+    						printf("a resposta eh %f \n" ,s);
+    					}
+    						else{
+    							 if (!strcmp(resp,"/")){
+    								printf("digite o 1 numero \n");
+    								scanf("%f", &a);
+    								printf("digite o 2 numero \n");
+   									 scanf("%f", &b);
+  									 s=a/b;
+    								printf("a resposta eh %f \n" ,s);    							  						
+    							}
+
+    						}
+
+
+    				}
+
+    		} 
+
+
+    }
+
+   
+    
+    
+	
+	return 0;
+}
