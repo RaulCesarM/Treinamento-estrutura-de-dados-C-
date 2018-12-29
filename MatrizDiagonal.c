ceb7b07c033878e780be549ff2132b527ab772fc
RaulCesar777 <raulzito737@gmail.com>
2018-12-29 21:28:54 -0200

    Update MatrizDiagonal.c
    ATERAÇÃO NOS COMENTARIOS

--- MatrizDiagonal.c
+++ MatrizDiagonal.c
@@ -1,50 +1,43 @@
+
+/* AUTOR : RAUL CESAR MULERSCHAT */
+
+
+
+
+
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 #include <conio.h>
-#define tam 2
+#define tam 2                                  /* definir as varivais no inicio facilitam a manipulação */
 
 
 int main(){
-
-
-	int matriz[tam][tam];
+	int matriz[tam][tam];							   /* tamnho da matriz é definido pela variavel tam */
 	int l, c;
-
 	for (l=0; l<tam; l++)
 	 {
 	 	for (c=0; c<tam; c++)
 	 	{
 	 		printf("digite da linha%d e coluna %d :\n\n ",l+1, c+1);
-	 		scanf("%d",&matriz[l][c]);
+	 		scanf("%d",&matriz[l][c]); 					   /* scaneia a matriz absorvendo as informações */
 	 	}
 	 }
-
-	
-	
-
 	for (l=0; l<tam; l++)
 	{	
 		for (c=0; c<tam; c++)
 		{			
 			
-			if (c==l) /* serve para comparar se os valores de linha e coluna são os mesmos */
-			{
-				
-						
-						for (c=l; c<tam; c++)/*começa com ovalor da linha e percorre a coluna */
-						{
-							printf("%d \t",matriz[l][c] );
+			if (c==l) 		    	/* serve para comparar se os valores de linha e coluna são os mesmos */
+			{							
+				for (c=l; c<tam; c++)					/*começa com ovalor da linha e percorre a coluna */
+				{
+					printf("%d \t",matriz[l][c] );      						    /* imprimi a matriz  */
 							
-						}
-						c=l;/* reinicia o estado natural da coluna */
-				
-					
-
-					
+				}
+				c=l;											  /* reinicia o estado natural da coluna */			
 				
 			}
-			printf("\t");
+			printf("\t");															  /* tabula a matriz */
 		}
 		printf("\n");
 	}
