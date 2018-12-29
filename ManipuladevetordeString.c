c6cba6b7448a7215f369ee227cd59137c747df63
RaulCesar777 <raulzito737@gmail.com>
2018-12-28 22:23:19 -0200

    Update ManipuladevetordeString.c

--- ManipuladevetordeString.c
+++ ManipuladevetordeString.c
@@ -1,44 +1,44 @@
-/* AUTOR: RAUL CESAR MULERSCHAT              */
+/* AUTOR: RAUL CESAR MULERSCHAT             */
 /*											*/
 /* RECEBE UMA PALAVRA,						*/
 /* DEVOLVE A PALAVRA DE TRAZ PARA FRENTE,	*/
 /* SUBSTITUI AS VOGAIS POR @ 				*/
 
 
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
+# include <stdio.h>                                            /* biblioteca para entrada e saida padrão */
+# include <string.h>                                           /* necessario para manipulação de Strings */
+# include <stdlib.h>                                                                /* biblioteca padrão */
 
 
-int main ()
+int main ()                                                             /* inicio do programa principal */
 {
-  int tam, i, j;
-  char nome[20];
-  char vogais[10]= "aeiouAEIOU";
-  printf("digite a string\n");
+  int tam, i, j;                           /* declaração da variaveis usadas nos laços de repetição loop */
+  char nome[20];                                                          /* vetor nome de 20 caracteres */
+  char vogais[10]= "aeiouAEIOU";                                             /* vetor contendo as vogais */
+  printf("digite a string\n");                                                   /* saida padrão na tela */
 
-  gets(nome);
+  gets(nome);                                                                           /* recebe o nome */
 
-  system("cls");
-  tam=strlen(nome);
+  system("cls");                                                           /* limpa tela pertence a STLIB*/
+  tam=strlen(nome);                                                       /*calcula o tamanho da palavra */
   printf("a sua string eh :\n");
-   for (i=0; i<tam; i++)
+   for (i=0; i<tam; i++)                            /* usa a variavel tam para definir o tamanho do loop */
     {         
-      printf(" %c",nome[i]);      
+      printf(" %c",nome[i]);                               /* imprime caracter a caracter,  char  a char */ 
     }
   printf("\nde traz para frente:\n");	
-    for (i=tam-1; i>-1; i--)
+    for (i=tam-1; i>-1; i--)             /*  no loop usa-se o tamnho final como inicial invertendo o vetor*/
     {
-    	printf(" %c",nome[i]);    	
+    	printf(" %c",nome[i]);                          	/* imprime caracter a caracter,  char  a char */ 
     }
     printf("\nde traz para frente com @ nas vogais:\n");
     for (i=0; i<tam; i++)
     {      
       for (j=0; j<10; j++)
       {
-        if (vogais[j]==nome[i])
+        if (vogais[j]==nome[i])                /*  a verificação passo-a-passo se a string contem vogais */
         {
-          nome[i]='@';
+          nome[i]='@';                                                   /* substitição da vogal por  @  */
         }
       }
     }
@@ -49,4 +49,4 @@ int main ()
     printf("\n");
 
 	return 0;
-}
+}                                                                         /* final do programa principal */
