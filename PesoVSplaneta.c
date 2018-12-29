addcbe3fcd5358149fe779fc558a03b337ca9dd6
RaulCesar777 <raulzito737@gmail.com>
2018-12-29 21:56:04 -0200

    Update PesoVSplaneta.c
    dfg

--- PesoVSplaneta.c
+++ PesoVSplaneta.c
@@ -1,3 +1,10 @@
+
+/* AUTOR:RAUL CESAR MULERSCHAT */
+/* CREATED IN 29/12/2018*/
+/* uso de switch, case para criação de menus */
+
+
+
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
@@ -20,37 +27,30 @@ void menu(){
 	scanf("%f", &pterra);
 	switch(op){
 		case 1:
-		printf("seu peso em mercurio %2.f kg\n",(pterra/1)*0.38);
+		printf("seu peso em mercurio %2.f kg\n",(pterra)*0.38);
 		break;
 		case 2:
-		printf("seu peso em venus %2.f kg\n",(pterra/1)*0.91);
+		printf("seu peso em venus %2.f kg\n",(pterra)*0.91);
 		break;
 		case 3:
-		printf("seu peso marte %2.f kg\n",(pterra/1)*0.38);
+		printf("seu peso marte %2.f kg\n",(pterra)*0.38);
 		break;
 		case 4:
-		printf("seu peso jupiter %2.f kg\n",(pterra/1)*2.34);
+		printf("seu peso jupiter %2.f kg\n",(pterra)*2.34);
 		break;
 		case 5:
-		printf("seu peso saturno %2.f kg \n",(pterra/1)*1.06);
+		printf("seu peso saturno %2.f kg \n",(pterra)*1.06);
 		break;
 		case 6:
-		printf("seu peso urano %2.f kg \n",(pterra/1)*1.19);
+		printf("seu peso urano %2.f kg \n",(pterra)*1.19);
 		break;
 		default:
 		printf("opcao inexistente");
 		break;
-
 	}
-
-
 }
-
-
 int main()
-{
-	
+{	
  menu();
-
 	return 0;
 }
