b0617c4bb0bd9e2b097eeaa70610614fe6f17dc5
RaulCesar777 <raulzito737@gmail.com>
2018-12-29 21:35:41 -0200

    Update MatrizDiagonal.c
    DFGH

--- MatrizDiagonal.c
+++ MatrizDiagonal.c
@@ -5,17 +5,17 @@
 #include <stdio.h>
 #include <math.h>
 #include <conio.h>
-#define tam 2                                 /* definir as varivais no inicio facilitam a manipulação */
+#define tam 2 									 /* definir as varivais no inicio facilitam a manipulação */
 
 int main(){
-	int matriz[tam][tam];							   /* tamnho da matriz é definido pela variavel tam */
+	int matriz[tam][tam];							   	/* tamnho da matriz é definido pela variavel tam */
 	int l, c;
 	for (l=0; l<tam; l++)
 	 {
 	 	for (c=0; c<tam; c++)
 	 	{
 	 		printf("digite da linha%d e coluna %d :\n\n ",l+1, c+1);
-	 		scanf("%d",&matriz[l][c]); 					   /* scaneia a matriz absorvendo as informações */
+	 		scanf("%d",&matriz[l][c]);						/* scaneia a matriz absorvendo as informações */
 	 	}
 	 }
 	for (l=0; l<tam; l++)
@@ -23,11 +23,11 @@ int main(){
 		for (c=0; c<tam; c++)
 		{			
 			
-			if (c==l) 		    	/* serve para comparar se os valores de linha e coluna são os mesmos */
+			if (c==l)				/* serve para comparar se os valores de linha e coluna são os mesmos */
 			{							
 				for (c=l; c<tam; c++)					/*começa com ovalor da linha e percorre a coluna */
 				{
-					printf("%d \t",matriz[l][c] );      						    /* imprimi a matriz  */
+					printf("%d \t",matriz[l][c] );									/* imprimi a matriz  */
 							
 				}
 				c=l;											  /* reinicia o estado natural da coluna */			
