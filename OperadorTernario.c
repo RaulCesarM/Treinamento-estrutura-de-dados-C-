2c4f326d81c017fd233939e7050211920c1973c2
RaulCesar777 <raulzito737@gmail.com>
2019-01-15 16:39:36 -0200

    teste

---
+++ OperadorTernario.c
@@ -0,0 +1,21 @@
+/**
+ * @author Raul Cesar Mulerschat
+ * @email raulzito737@gmail.com
+ * @create date 2019-01-13 23:06:01
+ * @modify date 2019-01-13 23:06:01
+ * @desc [OPERADOR TERNARIO]
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <windows.h>
+#include <string.h>
+
+
+int main(){
+    float nota ;
+    printf("digite nota\n");
+    scanf("%f",&nota);
+    printf( "%s\n", nota >= 6.0 ? "Aprovado" : "Reprovado" );
+    return 0;
+}
