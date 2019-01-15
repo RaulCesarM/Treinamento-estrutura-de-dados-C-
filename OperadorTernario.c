504cc6b23ac382555e61584e912ee4950b3f3322
RaulCesar777 <raulzito737@gmail.com>
2019-01-15 16:42:17 -0200

    teste

--- OperadorTernario.c
+++ OperadorTernario.c
@@ -3,7 +3,7 @@
  * @email raulzito737@gmail.com
  * @create date 2019-01-13 23:06:01
  * @modify date 2019-01-13 23:06:01
- * @desc [OPERADOR TERNARIO]
+ * @desc [OPERADOR TERNARIO "?"]
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,5 +17,10 @@ int main(){
     printf("digite nota\n");
     scanf("%f",&nota);
     printf( "%s\n", nota >= 6.0 ? "Aprovado" : "Reprovado" );
+
+
+    printf("digite outra nota\n");
+    scanf("%f",&nota);
+    nota >= 60 ? printf( "Aprovado\n" ) : printf( "Reprovado\n" );
     return 0;
 }
