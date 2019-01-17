c3cf6a2200fe30af5629b69af48e7ecc8fbff397
RaulCesar777 <raulzito737@gmail.com>
2019-01-17 21:56:56 -0200

    somando com o operador ao lado esquerdo da igualdade

--- Exercicios/Somatoriocomfor.c
+++ Exercicios/Somatoriocomfor.c
@@ -6,7 +6,7 @@ int soma =0;
 int numero;
 
     for (numero =2; numero<=100; numero +=2){//incrmenta o contador numero de 2 em 2 que seri a mesma coisa que numero= numero +2
-    soma=soma+numero; //adiciona numero a soma seria mesma coisa que soma=soma +numero
+    soma+=numero; //adiciona numero a soma seria mesma coisa que soma=soma +numero
     }
     printf("Soma é %d\n", soma);
 
